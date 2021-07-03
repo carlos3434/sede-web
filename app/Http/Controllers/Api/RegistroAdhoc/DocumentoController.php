f64b5d05915232dbe40915361f506c3ea1a945d7
jua rojas <carlos3434@hotmail.com>
2021-07-03 05:48:21 -0500

    modulo de CV: experiencias verificaciones

--- app/Http/Controllers/Api/RegistroAdhoc/DocumentoController.php
+++ app/Http/Controllers/Api/RegistroAdhoc/DocumentoController.php
@@ -5,15 +5,18 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Repositories\Auth\Interfaces\UserRepositoryInterface;
 use Illuminate\Support\Facades\Auth;
+use App\Models\Auth\User;
 
+use App\Helpers\FileUploader;
 class DocumentoController extends Controller
 {
     private $repository;
     private $fileUploader;
 
-    public function __construct(UserRepositoryInterface $repository)
+    public function __construct(UserRepositoryInterface $repository, FileUploader $fileUploader )
     {
         $this->repository = $repository;
+        $this->fileUploader = $fileUploader;
         $this->middleware(['role_or_permission:ADMINISTRADOR|USUARIO_DOCUMENTO_SHOW'])->only('index');
     }
 
@@ -23,8 +26,33 @@ class DocumentoController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show(User $user)
+    public function index()
     {
-        return $this->userRepository->getOne($user);
+        return $this->repository->getOne(Auth::user());
+    }
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\ExperienciaInspector  $experienciaInspector
+     * @return \Illuminate\Http\Response
+     */
+    public function update(ExperienciaInspectorUpdateRequest $request, ExperienciaInspector $experienciaInspector)
+    {
+        $all = $request->all();
+        $all = $this->storeFile($request, $all, 'experienciaInspector', 'archivo_constancia');
+        $experienciaInspector = $this->repository->updateOne($all, $experienciaInspector);
+        return response()->json($experienciaInspector, 200);
+    }
+
+    private function storeFile( $request , $all , $folder, $fieldName ){
+        if ( $request->hasFile($fieldName) ) {
+            $fileValue = $this->fileUploader->upload(
+                $request->file($fieldName),
+                'files/'.$folder
+            );
+            $all[$fieldName] = $folder.'/'.$fileValue;
+        }
+        return $all;
     }
 }
