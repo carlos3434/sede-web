d187bc6a4478f88290c812e1e670c982226a0603
Juan <Juan.C.Rojas.-ND@disney.com>
2021-07-06 18:42:27 -0500

    hay_convocatoria_actual variable booleana para determinar si hay convocatoria actual

--- app/Http/Controllers/Api/Listas/ParaPostulacionController.php
+++ app/Http/Controllers/Api/Listas/ParaPostulacionController.php
@@ -9,17 +9,19 @@ use App\Models\Settings\Institucion;
 use App\Http\Resources\Listas\ParaPostulacion\SedeRegistralCollection;
 use App\Models\Settings\SedeRegistral;
 use Illuminate\Support\Facades\Auth;
+use App\Models\Settings\Convocatoria;
 
 class ParaPostulacionController extends Controller
 {
     public function __construct()
     {
-        $this->middleware(['role_or_permission:ADMINISTRADOR|POSTULACION_INDEX'])->only('index');
+        //$this->middleware(['role_or_permission:ADMINISTRADOR|POSTULACION_INDEX'])->only('index');
     }
 
     public function index()
     {
         $response = [
+            'hay_convocatoria_actual' => (bool) Convocatoria::GetActual(),
             'esta_postulando' => Auth::user()->estaPostulando(),
             'sedes_registrales' => new SedeRegistralCollection(SedeRegistral::all())
         ];
