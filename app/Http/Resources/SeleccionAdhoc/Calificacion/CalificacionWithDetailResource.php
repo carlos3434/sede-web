22aeb7035a54d7e8d6a1824e61a3787fd62b243e
Juan <Juan.C.Rojas.-ND@disney.com>
2021-07-10 23:53:53 -0500

    validacion de acredicationes y puntajes, agregar nombre de grados e instituciones en listado de formaicones

--- app/Http/Resources/SeleccionAdhoc/Calificacion/CalificacionWithDetailResource.php
+++ app/Http/Resources/SeleccionAdhoc/Calificacion/CalificacionWithDetailResource.php
@@ -6,9 +6,9 @@ use Illuminate\Http\Resources\Json\JsonResource;
 use Carbon\Carbon;
 use App\Http\Resources\RegistroAdhoc\Formacion\FormacionResource;
 use App\Http\Resources\RegistroAdhoc\Capacitacion\CapacitacionResource;
-use App\Http\Resources\RegistroAdhoc\ExperienciasGenerales\ExperienciasGeneralesResource;
-use App\Http\Resources\RegistroAdhoc\ExperienciasInspector\ExperienciasInspectorResource;
-use App\Http\Resources\RegistroAdhoc\VerificacionesRealizadas\VerificacionesRealizadas;
+use App\Http\Resources\RegistroAdhoc\ExperienciaGenerales\ExperienciaGeneralesResource;
+use App\Http\Resources\RegistroAdhoc\ExperienciaInspector\ExperienciaInspectorResource;
+use App\Http\Resources\RegistroAdhoc\VerificacionRealizada\VerificacionRealizadaResource;
 class CalificacionWithDetailResource extends JsonResource
 {
     /**
@@ -25,9 +25,9 @@ class CalificacionWithDetailResource extends JsonResource
             'usuario_id'             => $this->usuario_id,
             'formaciones'            => new FormacionResource($this->user->formaciones),
             'capacitaciones'         => new CapacitacionResource($this->user->capacitaciones),
-            'experiencias_generales'        => new CapacitacionResource($this->user->experienciasGenerales),
-            'experiencias_inspector'        => new ExperienciasInspectorResource($this->user->experienciasInspector),
-            'verificaciones_realizadas'     => new VerificacionesRealizadas($this->user->verificacionesRealizadas),
+            'experiencias_generales'        => new ExperienciaGeneralesResource($this->user->experienciasGenerales),
+            'experiencias_inspector'        => new ExperienciaInspectorResource($this->user->experienciasInspector),
+            'verificaciones_realizadas'     => new VerificacionRealizadaResource($this->user->verificacionesRealizadas),
             'convocatoria_id'        => $this->convocatoria_id,
             'convocatoria_nombre'    => $this->convocatoria->nombre,
             'sede_registral_id'      => $this->sede_registral_id,
