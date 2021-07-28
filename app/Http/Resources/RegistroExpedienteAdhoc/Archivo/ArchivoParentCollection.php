02013e0d4e173e70d6d17e850c304168b96b4e99
Juan <Juan.C.Rojas.-ND@disney.com>
2021-07-27 21:39:31 -0500

    config supervisor

--- app/Http/Resources/RegistroExpedienteAdhoc/Archivo/ArchivoParentCollection.php
+++ app/Http/Resources/RegistroExpedienteAdhoc/Archivo/ArchivoParentCollection.php
@@ -4,7 +4,7 @@ namespace App\Http\Resources\RegistroExpedienteAdhoc\Archivo;
 
 use Illuminate\Http\Resources\Json\ResourceCollection;
 
-class ExpedienteAdhocArchivoResource extends ResourceCollection
+class ArchivoParentCollection extends ResourceCollection
 {
     /**
      * Transform the resource collection into an array.
