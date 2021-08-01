ef4829a63f1763991e6e66538d2795983169e9bd
Juan <Juan.C.Rojas.-ND@disney.com>
2021-08-01 18:52:11 -0500

    generar items en seeder en base a una convocatoria actual

--- database/seeds/ItemSeeder.php
+++ database/seeds/ItemSeeder.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Seeder;
 use App\Models\SeleccionAdhoc\Item;
+use App\Models\Settings\Convocatoria;
 
 class ItemSeeder extends Seeder
 {
@@ -12,38 +13,47 @@ class ItemSeeder extends Seeder
      */
     public function run()
     {
+        $convId = (isset( Convocatoria::GetActual()->id )) ? Convocatoria::GetActual()->id : false;
+        if ( !$convId ) {
+            $conv = Convocatoria::create([
+                'nombre'      => 'CONVOCATORIA_2021_ACTUAL',
+                'fecha_inicio'     => '2021-01-01',
+                'fecha_final'     => '2021-12-30',
+            ]);
+            $convId = $conv->id;
+        }
         //Formacion
         Item::create([
             'nombre' => 'Titulado (03 Puntos)',
             'puntaje' => '3',
             'categoria_id' => 1,
-            'convocatoria_id' => '1',
+            'convocatoria_id' => $convId,
         ]);
         Item::create([
             'nombre' => 'Postgrado (04 puntos)',
             'puntaje' => '4',
             'categoria_id' => 1,
-            'convocatoria_id' => '1',
+            'convocatoria_id' => $convId,
         ]);
         Item::create([
             'nombre' => 'Magister o Doctorado (05 puntos)',
             'puntaje' => '5',
             'categoria_id' => 1,
-            'convocatoria_id' => '1',
+            'convocatoria_id' => $convId,
         ]);
 
         //Capacitacion
         Item::create([
             'nombre' => '20 horas lectivas (1 punto)',
-            'puntaje' => '1',
+            'puntaje' => $convId,
             'categoria_id' => 2,
-            'convocatoria_id' => '1',
+            'convocatoria_id' => $convId,
         ]);
         Item::create([
             'nombre' => '60 horas lectivas (2 puntos)',
             'puntaje' => '2',
             'categoria_id' => 2,
-            'convocatoria_id' => '1',
+            'convocatoria_id' => $convId,
         ]);
 
         //ExperienciaGeneral
@@ -51,33 +61,33 @@ class ItemSeeder extends Seeder
             'nombre' => '06 a 07 años (03 Puntos)',
             'puntaje' => '3',
             'categoria_id' => 3,
-            'convocatoria_id' => '1',
+            'convocatoria_id' => $convId,
         ]);
         Item::create([
             'nombre' => '07 a 09 años (04 puntos)',
             'puntaje' => '4',
             'categoria_id' => 3,
-            'convocatoria_id' => '1',
+            'convocatoria_id' => $convId,
         ]);
         Item::create([
             'nombre' => '10 años a más (05 puntos)',
             'puntaje' => '5',
             'categoria_id' => 3,
-            'convocatoria_id' => '1',
+            'convocatoria_id' => $convId,
         ]);
 
         //ExperienciaInspector
         Item::create([
             'nombre' => '05 a 10 años (01 Punto)',
-            'puntaje' => '1',
+            'puntaje' => $convId,
             'categoria_id' => 4,
-            'convocatoria_id' => '1',
+            'convocatoria_id' => $convId,
         ]);
         Item::create([
             'nombre' => 'más de 10 años (02 puntos)',
             'puntaje' => '2',
             'categoria_id' => 4,
-            'convocatoria_id' => '1',
+            'convocatoria_id' => $convId,
         ]);
 
         //VerificacionesRealizadas
@@ -85,19 +95,19 @@ class ItemSeeder extends Seeder
             'nombre' => '1 a 10 verificaciones (02 Punto)',
             'puntaje' => '2',
             'categoria_id' => 5,
-            'convocatoria_id' => '1',
+            'convocatoria_id' => $convId,
         ]);
         Item::create([
             'nombre' => '11 a 15 verificaciones (04 puntos)',
             'puntaje' => '4',
             'categoria_id' => 5,
-            'convocatoria_id' => '1',
+            'convocatoria_id' => $convId,
         ]);
         Item::create([
             'nombre' => 'más de 15 verificaciones (06 puntos)',
             'puntaje' => '6',
             'categoria_id' => 5,
-            'convocatoria_id' => '1',
+            'convocatoria_id' => $convId,
         ]);
     }
 }
