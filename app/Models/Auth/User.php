0417761a7d0a5bfb94f3a7a98495d7b9ffe5c53c
juan carlos rojas toralva <carlos3434@hotmail.com>
2022-01-24 15:29:16 -0500

    Update User.php

--- app/Models/Auth/User.php
+++ app/Models/Auth/User.php
@@ -17,7 +17,7 @@ class User extends Authenticatable //implements MustVerifyEmail
     use HasApiTokens;
     use Notifiable;
     use HasRoles;
-    use LogsActivity;
+    //use LogsActivity;
 
     /**
      * The attributes that are mass assignable.
